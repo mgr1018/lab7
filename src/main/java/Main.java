e2c31206f339a30a91faa9ee059fb1c10a48392a
mgr1018 <mg1018@ic.ac.uk>
2020-12-02 17:11:14 +0000

    commit v1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello world!");
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+        int c = 30;
+        for (int i = 31; i < 41; i++) {
+            c = c+i;
+        }
+        System.out.println(c);
+
     }
 
 }
